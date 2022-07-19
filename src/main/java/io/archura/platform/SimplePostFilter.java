45ff40bd2ea804d0902b64e1447c09e387ee2d2b
Ali Can Mogol <canmogol@gmail.com>
2022-07-19 16:55:20 +0200

    Add Context and related classes.

--- src/main/java/io/archura/platform/SimplePostFilter.java
+++ src/main/java/io/archura/platform/SimplePostFilter.java
@@ -1,37 +1,33 @@
 package io.archura.platform;
 
+import io.archura.platform.context.Context;
 import io.archura.platform.function.Configurable;
+import io.archura.platform.logging.Logger;
 import org.springframework.http.HttpHeaders;
 import org.springframework.stereotype.Component;
 import org.springframework.web.servlet.function.ServerRequest;
 import org.springframework.web.servlet.function.ServerResponse;
 
-import java.util.Date;
 import java.util.Map;
 import java.util.function.BiConsumer;
 
 @Component
 public class SimplePostFilter implements BiConsumer<ServerRequest, ServerResponse>, Configurable {
 
+    private Map<String, Object> configuration;
+
     @Override
-    public void accept(ServerRequest serverRequest, ServerResponse serverResponse) {
-        log("serverRequest = " + serverRequest + " serverResponse = " + serverResponse);
-        final HttpHeaders headers = serverResponse.headers();
-        headers.entrySet().forEach(stringListEntry -> log("stringListEntry = " + stringListEntry));
+    public void accept(ServerRequest request, ServerResponse response) {
+        final Context context = (Context) request.attributes().get(Context.class.getSimpleName());
+        final Logger logger = context.getLogger();
+
+        logger.info("request = " + request + " response = " + response + " configuration = " + configuration);
+        final HttpHeaders headers = response.headers();
+        headers.entrySet().forEach(stringListEntry -> logger.info("stringListEntry = " + stringListEntry));
     }
 
     @Override
     public void setConfiguration(Map<String, Object> configuration) {
-        log("configuration = " + configuration);
-    }
-
-    private void log(final String log) {
-        String message = String.format("[%s] [%s-%s] [%s]: %s",
-                new Date(),
-                Thread.currentThread().getId(),
-                Thread.currentThread().getName(),
-                this.getClass().getSimpleName(),
-                log);
-        System.out.println(message);
+        this.configuration = configuration;
     }
 }
