5af9cbb5a6b1d5617944ce9d0675edf4ad00d94b
Can MOGOL <canmogol@gmail.com>
2022-10-11 22:09:07 +0200

    Remove spring dependency.

--- src/main/java/io/archura/platform/imperativeshell/post/filter/SimplePostFilter.java
+++ src/main/java/io/archura/platform/imperativeshell/post/filter/SimplePostFilter.java
@@ -1,28 +1,27 @@
 package io.archura.platform.imperativeshell.post.filter;
 
 import io.archura.platform.api.context.Context;
+import io.archura.platform.api.http.HttpServerRequest;
+import io.archura.platform.api.http.HttpServerResponse;
 import io.archura.platform.api.logger.Logger;
 import io.archura.platform.api.type.Configurable;
-import org.springframework.http.HttpHeaders;
-import org.springframework.stereotype.Component;
-import org.springframework.web.servlet.function.ServerRequest;
-import org.springframework.web.servlet.function.ServerResponse;
 
+import java.util.List;
 import java.util.Map;
 import java.util.function.BiFunction;
 
-@Component
-public class SimplePostFilter implements BiFunction<ServerRequest, ServerResponse, ServerResponse>, Configurable {
+
+public class SimplePostFilter implements BiFunction<HttpServerRequest, HttpServerResponse, HttpServerResponse>, Configurable {
 
     private Map<String, Object> configuration;
 
     @Override
-    public ServerResponse apply(ServerRequest request, ServerResponse response) {
-        final Context context = (Context) request.attributes().get(Context.class.getSimpleName());
+    public HttpServerResponse apply(HttpServerRequest request, HttpServerResponse response) {
+        final Context context = (Context) request.getAttributes().get(Context.class.getSimpleName());
         final Logger logger = context.getLogger();
 
         logger.info("request = " + request + " response = " + response + " configuration = " + configuration);
-        final HttpHeaders headers = response.headers();
+        Map<String, List<String>> headers = response.getHeaders();
         headers.entrySet().forEach(stringListEntry -> logger.info("stringListEntry = " + stringListEntry));
         return response;
     }
